a0132b313d874d530168678e295b2117dc75d443
Code Abe <109777019+jrick2@users.noreply.github.com>
2023-04-08 22:56:00 -0500

    Update UserWidget.jsx

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -22,7 +22,7 @@ const UserWidget = ({ userId, picturePath }) => {
   const main = palette.neutral.main;
 
   const getUser = async () => {
-    const response = await fetch(`https://server-blog-post-app.onrender.com/${userId}`, {
+    const response = await fetch(`https://server-blog-post-app.onrender.com/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
